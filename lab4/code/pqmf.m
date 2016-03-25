8b3c0449f9ed19a83a9fc43f0f5b183719602ba2
erik-feller <erik.feller@colorado.edu>
2016-03-25 15:48:41 +0000

    Cleaning up code. Removed storage of samples

--- lab4/code/pqmf.m
+++ lab4/code/pqmf.m
@@ -5,7 +5,6 @@ function [ S ] = pqmf( filename, frameSize )
 [audio, fs] = audioread(filename);
 nFrame = floor (length (audio)/frameSize);
 buffer = zeros(1,512);
-song_data = zeros(18,32,nFrame); %This may be un-needed. Revisit
 window = loadwindow;
 S = zeros(32,nFrame);
     for frame = 1:nFrame                % chunk the audio into blocks of 576 samples
@@ -14,18 +13,17 @@ S = zeros(32,nFrame);
         for index = 1:18   
             total_offset = (offset+(index-1)*32)+1;
             buffer = circshift(buffer,32,2);% shift old data to front
-            buffer(1:32) = fliplr(audio(total_offset:total_offset+31)); %replace oldest data
+            buffer(1:32) = fliplr(audio(total_offset:total_offset+31)); %replace oldest data flip for conv
             Z = buffer.*window; %Calculate Z
             Y = zeros(1,64); %Create a place for Y
             for i = 1:size(buffer,2)
-                Y(mod(i-1,64)+1) = Y(mod(i-1,64)+1) + buffer(i);
+                Y(mod(i-1,64)+1) = Y(mod(i-1,64)+1) + buffer(i); %Build Y
             end
             for k = 1:32
                 for r = 1:size(Y,2)
-                    S(k,frame) = S(k,frame) + (cos(((2*k)*(r-17)*pi)/64)*Y(r));
+                    S(k,frame) = S(k,frame) + (cos(((2*k)*(r-17)*pi)/64)*Y(r)); %Build S
                 end
             end
-            %song_data(segment,:,frame) = audio(total_offset:total_offset+31);%This may be uneeded. Revisit
         end
     end
 
