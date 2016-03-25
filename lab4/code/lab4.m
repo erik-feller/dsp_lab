4f2db5909aebc2d87fe549cf5ada0d2d91300a3e
erik-feller <erik.feller@colorado.edu>
2016-03-25 13:39:08 +0000

    got the chunking working

--- lab4/code/lab4.m
+++ lab4/code/lab4.m
@@ -5,4 +5,4 @@
 % This harness calls all of the necessary functions for lab 4
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
+pqmf('../lab4-data/sample1.wav',576)
--- lab4/code/pqmf.m
+++ lab4/code/pqmf.m
@@ -1,20 +1,25 @@
-function [ out ] = pqmf( song_location )
+function [ song_data ] = pqmf( filename, frameSize )
 %pqmf: filter for mp3
 %   Detailed explanation goes here
 
-[audio, fs] = wavread (filename);
-frameSize = 576;
+[audio, fs] = audioread(filename);
 nFrame = floor (length (audio)/frameSize);
+buffer = zeros(1,512);
+song_data = zeros(18,32,nFrame);
     for frame = 1:nFrame                % chunk the audio into blocks of 576 samples
-        offset = (frame - 1)* frameSize;       % absolute address of the frame
-        for index = 1:18                  % 18 non overlapping blocks of size 32
+        for segment = 1:18
+            
+        offset = (frame - 1)*frameSize;       % absolute address of the frame
+        for index = 1:18    % 18 non overlapping blocks of size 32
+            total_offset = (offset+(index-1)*32)+1;
+            song_data(segment,:,frame) = audio(total_offset:total_offset+31);
             % process a block of 32 new input samples
             % see flow chart in Fig. 
             % Frequency inversion
-            if (mod (index,2) == 1)
-                channel = 1:2:32;
-                S(channel) = -S(channel);
-            end
+%             if (mod (index,2) == 1)
+%                 channel = 1:2:32;
+%                 S(channel) = -S(channel);
+%             end
         end
     end
 
