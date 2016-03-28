f41f4c1862b38855aee63e50a5d2ced5e37ea299
erik-feller <erik.feller@colorado.edu>
2016-03-28 08:17:51 +0000

    Struggling to find a reason

--- lab4/code/lab4.m
+++ lab4/code/lab4.m
@@ -5,6 +5,6 @@
 % This harness calls all of the necessary functions for lab 4
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-S = pqmf('../lab4-data/sample2.wav',576);
+S = pqmf('../lab4-data/sample1.wav',576,5);
 m = ipqmf(S);
 
--- lab4/code/pqmf.m
+++ lab4/code/pqmf.m
@@ -1,9 +1,20 @@
-function [ S ] = pqmf( filename, frameSize)
+function [ S ] = pqmf( filename, frameSize, dur)
 %pqmf: filter for mp3
-%   Detailed explanation goes here
+%   filename is the file location
+%   frameSize is the size of frame to be used
+%   length is the length of the sample to be used. Input sanitized
 
 [audio, fs] = audioread(filename);
-audio = audio(1:fs*5);
+if size(audio,1) > dur*fs
+    audio = audio(1:dur*fs);
+else
+    disp('length longer than audio track, using whole track...');
+end
+%%
+%Plot the original audio
+figure(1);
+plot(audio);
+
 nFrame = floor (length (audio)/frameSize);
 buffer = zeros(1,512); %Make circular buffer
 window = loadwindow;
@@ -11,13 +22,13 @@ S = zeros(32,nFrame*18); %Create S to hold a number for each computation
 for frame = 1:nFrame                % chunk the audio into blocks of 576 samples   
     offset = (frame - 1)*frameSize;       % absolute address of the frame
     for index = 1:18   
-        total_offset = (offset+(index-1)*32)+1
+        total_offset = (offset+(index-1)*32)+1;
         buffer = circshift(buffer,32,2);% shift old data to front
         buffer(1:32) = fliplr(audio(total_offset:total_offset+31)); %replace oldest data flip for conv
         Z = buffer.*window; %Calculate Z (apply window)
         Y = zeros(1,64); %Create a place for Y
         for i = 1:size(buffer,2)
-            Y(mod(i-1,64)+1) = Y(mod(i-1,64)+1) + buffer(i); %Build Y
+            Y(mod(i-1,64)+1) = Y(mod(i-1,64)+1) + Z(i); %Build Y
         end
         for k = 1:32
             for r = 1:size(Y,2)
@@ -33,6 +44,7 @@ for frame = 1:nFrame                % chunk the audio into blocks of 576 samples
 end
 %%
 %plot things
+figure(2);
 plot(unwrap_s(S));
 
 end
