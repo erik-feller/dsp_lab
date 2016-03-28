c12e0ac1fde3659701c1b38a1c34dee72becf690
Erik Feller <erik.feller@colorado.edu>
2016-03-28 04:43:49 -0400

    Added band selection

---
+++ lab4/code/ipqmf_bands.m
@@ -0,0 +1,40 @@
+function [ audio ] = ipqmf( coef, bands )
+%ipqmf: inverse pqmf signal analysis
+%   S should be taken from pqmf function
+
+audio = zeros(1,size(coef,2)/18*576);
+buffer = zeros(1,1024); %make the buffer V
+U = zeros(1,512); %make pre windowed holder
+[C, D] = loadwindow; %load in window
+for N = 1:size(coef,2)
+    buffer = circshift(buffer,64,2);
+    buffer(1:64) = 0; %zero out the front of buffer so that the sum works well
+    for i = 0:63
+        for band = 1:size(bands,2) 
+            k = bands(band);
+            if mod(k,2) == 1
+                mul = -1;
+            else
+                mul = 1;
+            end
+            buffer(i+1) = buffer(i+1) + cos(((2.*k+1)*(16+i)*pi)/64).*coef(k+1,N)*mul;
+        end
+    end
+    %Make U
+    for i = 0:7
+        for j = 0:31
+            U(64*i+j+1) = buffer(128*i+j+1);
+            U(64*i+j+32+1) = buffer(128*i+j+96+1);
+        end
+    end
+    %window U
+    W = U.*D;
+    for j = 0:31
+        for i = 0:15
+            audio(j+(N-1)*32+1) = audio(j+(N-1)*32+1) + W(j+32*i+1);
+        end
+    end
+end
+
+end
+
