74c071262a849e9a720483a4084590f17d0898fb
erik-feller <erik.feller@colorado.edu>
2016-03-31 13:46:43 +0000

    minor changes

--- lab4/code/lab4.m
+++ lab4/code/lab4.m
@@ -5,7 +5,7 @@
 % This harness calls all of the necessary functions for lab 4
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-[frames, song] = framify('../lab4-data/gilberto.wav',576,5);
+[frames, song] = framify('../lab4-data/cast.wav',576,5);
 S = pqmf(frames);
 % S = pqmf('../lab4-data/sample2.wav',576);
 m = ipqmf(S);
@@ -24,7 +24,7 @@ err_calc(song', m)
 
 %%
 %decare bands and determine
-b = 0:15;
+b = 0:10;
 m_b = ipqmf_bands(S, b);
 
 %%
