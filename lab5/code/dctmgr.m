85e7b903dc17e00fbc0afeee07b01ed9f5f94a32
Erik Feller <erik.feller@colorado.edu>
2016-04-04 17:14:55 -0400

    Starting the DCT

---
+++ lab5/code/dctmgr.m
@@ -0,0 +1,36 @@
+function [ cos_tran ] = dctmgr( image_data )
+%dctmgr: provides cosine transformed image data as requested
+%   Detailed explanation goes here
+
+%%
+%Tilin the image into 8x8 chunks
+v_tiles = floor(size(image_data,1)/8);
+h_tiles = floor(size(image_data,2)/8);
+nTiles = v_tiles*h_tiles;
+tiles = zeros(8,8,nTiles);
+for v = 1:v_tiles
+    for h = 1:h_tiles
+        h_offset = (h-1)*8+1
+        v_offset = (v-1)*8+1
+        tiles(:,:,(v-1)*v_tiles+h) = image_data(v_offset:v_offset+7,h_offset:h_offset+7);
+    end
+end
+
+%%
+%Take the DCT of each block
+cos_tran = zeros(64,nTiles);
+for tile = 1:nTiles
+    %reorder into a zigzag
+    indices = reshape(1:numel(tiles(:,:,tile)), size(tiles(:,:,tile)));
+    indices_m = fliplr(spdiags(fliplr(indices)));
+    indices_m(:,1:2:end) = flipud(indices_m(:,1:2:end));
+    indices_m(indices_m==0) = [];
+    vals = tiles(:,:,tile);
+    vals = vals(indices_m);
+    cos_tran(:,tile) = vals';
+    for i = 1:64
+        
+end
+
+end
+
--- lab5/code/lab5.m
+++ lab5/code/lab5.m
@@ -5,3 +5,6 @@
 %
 % Harness to run functions written for lab 5
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+img = imread('../images/fingerprint.pgm');
+dctmgr(img);
